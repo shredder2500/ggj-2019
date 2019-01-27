1c9acc08f3c4bf181bcfd6e42518f983d36fc107
Christopher Taylor <shredder.ct@gmail.com>
2019-01-27 14:00:52 -0600

    sounds and music

---
+++ Assets/Scripts/CharacterBasedSound.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBasedSound : MonoBehaviour
+{
+    [SerializeField]
+    private AudioClip _catSound;
+    [SerializeField]
+    private AudioClip _dogSound;
+
+    private void Start() {
+        GetComponent<AudioSource>().clip = CharacterSelect.IsDog ? _dogSound : _catSound;
+    }
+}
